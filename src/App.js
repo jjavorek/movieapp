e649dda5b895d30c27361be8fbb732e6ff5d6438
jjavorek <justinjavorek@gmail.com>
2022-04-04 10:05:37 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
 
-function App() {
+// fc546608
+
+const API_URL = "http://www.omdbapi.com?apikey=fc546608";
+
+const App = () => {
+  const name = "Justin";
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {name}</h1>
     </div>
   );
-}
+};
 
 export default App;
